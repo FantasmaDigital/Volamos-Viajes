1102c7ec12bc5301c8e1a9e86f0d97c5af5858a9
DanielHrndz <00015322@uca.edu.sv>
2024-12-12 10:49:01 -0600

    add helmet SEO

--- Client/src/components/WhatsAppBtn.tsx
+++ Client/src/components/WhatsAppBtn.tsx
@@ -5,22 +5,22 @@ import { FRONT_URI } from "../constants/URIS";
 const WhatsAppBtn: React.FC = () => {
     const props = {
         text: "Contáctanos en WhatsApp",
-        number: "+50370200976",
-        company: "Volamos Viajes", // Agregamos el nombre de la empresa
+        number: "+50370200976", // Asegúrate de que este número esté correcto
+        company: "Volamos Viajes",
         message: "¿Tienes preguntas sobre nuestros destinos? 🤔🌍 \n¡Estamos aquí para ayudarte! 💬🙂",
-        avatar: `${/favicon.ico`
+        avatar: `${FRONT_URI}/favicon.ico`, // Verifica que la ruta sea correcta
     };
 
     return (
-        <FloatingWhatsApp 
+        <FloatingWhatsApp
             phoneNumber={props.number}
             accountName={props.company}
             chatMessage={props.message}
             avatar={props.avatar}
             style={{ 
-                bottom: '30px', // Ajusta la distancia desde el fondo
-                left: '30px',   // Ajusta la distancia desde el borde izquierdo
-                zIndex: 1000,   // Asegura que el botón quede por encima de otros elementos
+                bottom: '30px',  // Ajuste según el diseño
+                left: '30px',    // Ajuste según el diseño
+                zIndex: 1000,    // Para que el botón esté por encima de otros elementos
             }}
         />
     );
